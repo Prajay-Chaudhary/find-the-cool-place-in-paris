896dfb361044a8ca5ce8f883909a52b09c6d3cf2
Prajay Chaudhary <prajaychaudhary5678@gmail.com>
2023-11-18 14:12:28 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className='text-red-500 text-center'>test app</p>
+      </div>
+    </>
+  )
+}
+
+export default App
